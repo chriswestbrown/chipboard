f375a0fc04e4f7e6708ae28c5abd10ec0c4657b7
Chris Daves <m201362@usna.edu>
2019-07-23 11:55:26 -1000

    Minor appearance changes

--- tactical.py
+++ tactical.py
@@ -2,8 +2,7 @@ from board import LFPlay, Board
 import numpy
 
 class TacticalPlay(LFPlay):
-    def tacticalGreedy(self,V):
-        """Greedy choice function."""
+    def tacticalgreedy(self,V):
         s1 = self.countRemoved(V,0)/self.countRed(V,0)
         s2 = self.countRemoved(V,9)/self.countRed(V,9)
         return s2 - s1
@@ -16,20 +15,19 @@ class Tester:
         #results
         t = TacticalPlay()
         res = {"greedy": [], "other": []}
+        wins = {"greedy":[], "other":[]}
+
         for i in range(N):
             b = Board(6,140,.4)
             res["greedy"].append(t.playout(b.clone(),t.greedy))
             res["other"].append(t.playout(b.clone(),strat))
-
-        wins = {"greedy":[], "other":[]}
-        for i in range(N):
             if res["greedy"][i] < res["other"][i]:
                 wins["greedy"].append(i)
             else:
                 wins["other"].append(i)
 
-        print "other wins " + str(len(wins["other"]))
-        print "greedy wins " + str(len(wins["greedy"]))
+        print "other wins: " + str(len(wins["other"]))
+        print "greedy wins: " + str(len(wins["greedy"]))
         wavg = sum([res["greedy"][i]/res["other"][i] for i in wins["other"]])/len(wins["other"])
         lavg = sum([res["other"][i]/res["greedy"][i] for i in wins["greedy"]])/len(wins["greedy"])
 
