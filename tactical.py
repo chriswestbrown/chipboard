ed5d2dd01c9daed2d88c2077d101df0d2572a671
Chris Daves <m201362@usna.edu>
2019-07-23 11:02:52 -1000

    Updated tester class

--- tactical.py
+++ tactical.py
@@ -1,4 +1,5 @@
 from board import LFPlay, Board
+import numpy
 
 class TacticalPlay(LFPlay):
     def tacticalGreedy(self,V):
@@ -7,10 +8,21 @@ class TacticalPlay(LFPlay):
         s2 = self.countRemoved(V,9)/self.countRed(V,9)
         return s1 - s2
     def antitacticalgreedy(self,V):
-        return -1*tacticalGreedy(V)
+        return -1*self.tacticalGreedy(V)
 
 class Tester:
-    def testStrats(self,N):
-        g,ag,t,at = 0
+    def testStrats(self,N,strats):
+        t = TacticalPlay()
+        res = {s : [] for s in strats}
         for i in range(N):
             b = Board(6,140,.4)
+            for s in strats:
+                res[s].append(t.playout(b.clone(),s))
+        wins = {s : 0 for s in strats}
+        for i in range(N):
+            p = [ res[s][i] for s in strats]
+            m = min([res[s][i] for s in strats])
+            for s in strats:
+                if m == res[s][i]:
+                    wins[s] += 1
+        return wins
