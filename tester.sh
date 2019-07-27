29910749069d738e01ac4acfbff73e48610b61ec
HPC_MAUI <m201560@usna.edu>
2019-07-26 19:37:02 -0700

    the tester file used to check that python and cpp play out the same and produce the same boards/read in boards

--- tester.sh
+++ tester.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-Board = $(./chipboard -b)
-echo $Board | ./chipboard -p > cpp.txt
-echo $Board | python testPC.py > py.txt
-ret = $(diff cpp.txt py.txt)
-if [ $ret != "" ]; then
-  echo $ret
-fi
+for i in {0..$1}
+do
+  echo $i
+  ./chipboard -b > board.txt
+  ./chipboard -p < board.txt > cpp.txt
+  python -W ignore testPC.py < board.txt > py.txt
+  diff cpp.txt py.txt
+done
