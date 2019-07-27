6371d5f2b16ff6509efa8fdcef28b7cf289cc384
Chris Daves <m201362@usna.edu>
2019-07-26 16:15:06 -1000

    Testing

--- testPC.py
+++ testPC.py
@@ -1,5 +1,9 @@
 from learn import Learner
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+
 l = Learner(0.001)
 a = l.model.get_weights()
 a[0][0][0] = .25
@@ -9,6 +13,5 @@ a[0][3][0] = .25
 a[1][0] = .25
 l.model.set_weights(a)
 x,y = l.generateTestData(np.zeros((100,4)),np.zeros((100)),10,0)
-print len(x)
 for i in range(len(x)):
     print(str(str(int(x[i][0]))+", "+str(int(x[i][1]))+", "+str(int(x[i][2]))+", "+str(int(x[i][3]))+"\n"+str(int(y[i]))))
