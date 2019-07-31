4b000d48d6ee3c6b844bc26311b9107c331d8c66
HPC_MAUI <m201560@usna.edu>
2019-07-31 11:42:20 -0700

    added info to mpiHelloWorld

--- mpiHelloWorld.py
+++ mpiHelloWorld.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/env python
+
 #Parallel HelloWorld
 
 from mpi4py import MPI
@@ -8,7 +8,7 @@ import sys
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
-sys.stdout = open('file' + str(rank) + 'of' + str(size), 'w')
+sys.stdout = open(sys.argv[1] + '_file' + str(rank) + 'of' + str(size) +".txt", 'w')
 print(
     "Hello, World! I am process %d of %d on %s.\n"
     % (rank, size, name))
