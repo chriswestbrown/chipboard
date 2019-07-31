fdac3c205eb82fa97a506f4ed4adcd92790aabfe
Patrick Joseph Dowd <m201560@hokulea02.mhpcc.hpc.mil>
2019-07-31 12:40:28 -1000

    updated test

--- helloWorldTest.sh
+++ helloWorldTest.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=5:mpiprocs=5
+#PBS -l select=3:ncpus=5:mpiprocs=5
 #PBS -l place=scatter:excl
 #PBS -A MHPCC96650N19
 #PBS -q debug
-#PBS -l walltime=0:00:10
+#PBS -l walltime=0:01:00
+#PBS -o ${WORKDIR}/output.txt
+#PBS -e ${WORKDIR}/error.txt
 #PBS -N helloWorldTest
 
-
-export PYTHONWARNINGS="ignore"
-cd $WORKDIR
+cd ${WORKDIR}
 module purge
-module load pbs tensorflow/1.8.0 spectrum-mpi cuda/9.2
-mpirun ./mpiHelloWorld.py
+module load anaconda3/5.2.0
+source activate mpi4py
+module load gcc/5.3.0 cuda/9.2 openmpi/2.1.1/gnu/5.3.0
+mpirun python mpiHelloWorld.py
--- mpiHelloWorld.py
+++ mpiHelloWorld.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-
 #Parallel HelloWorld
-
-from mpi4py import MPI
 import sys
+from mpi4py import MPI
 
-size = MPI.COMM_WORLD.Get_size()
-rank = MPI.COMM_WORLD.Get_rank()
-name = MPI.Get_processor_name()
-sys.stdout = open(sys.argv[1] + '_file' + str(rank) + 'of' + str(size) +".txt", 'w')
-print(
-    "Hello, World! I am process %d of %d on %s.\n"
-    % (rank, size, name))
+def main():
+	size = MPI.COMM_WORLD.Get_size()
+	rank = MPI.COMM_WORLD.Get_rank()
+	name = MPI.Get_processor_name()
+	print(f"Hello, World! I am process {rank}  of {size} on {name}.txt")
+
+if __name__ == "__main__":
+	main()
