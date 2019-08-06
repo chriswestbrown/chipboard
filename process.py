19bfbe87f7dc3f1a25e5e1684465a20f50ece195
Glenn Christopher Daves <m201362@hokulea02.mhpcc.hpc.mil>
2019-08-06 10:23:48 -1000

    Hopefully its fixed

--- process.py
+++ process.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE
-p = Popen(['./mybc'],stdout=PIPE,stdin=PIPE)
-text = p.communicate("3+4\n")[0]
+p = Popen(['bc'],stdout=PIPE,stdin=PIPE)
+text = p.communicate("3+4\n".encode())[0]
 p.stdin.close()
-print text
+print(text.decode())
