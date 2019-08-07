21047abf76bdcd21d23edfc1954a2d48cfa06af8
HPC_MAUI <m201560@usna.edu>
2019-08-07 13:13:58 -0700

    updates to graphtest

--- graphtest.py
+++ graphtest.py
@@ -1,5 +1,5 @@
 
-def generateGraph(a):
+def generateGraph(a, weight, bias):
     graphString = ""
     layer={}
     i=1
@@ -7,13 +7,19 @@ def generateGraph(a):
         layer[i-1] = list(range(sum(a[0:i])-a[i-1], sum(a[0:i])))
         i = i+1
 
-    y=0
-    while y<len(layer):
-        for i in layer[y]:
-            if y==0:
-                graphString += ("(i:function:" + str(i) + ":" +  str(y) + ":" + str(i) + ":[])")
-            else:
-                graphString += ("(m:function:" + str(i) + ":" + str(y) + ":" + str(i-layer[y-1][-1]-1) + ":" + str(layer[y-1]) + ")")
-        y+=1
-
+    graphString=" "
+    x = 0;
+    while(x < len(layer)):
+        if x==0:
+            graphString += "( i function " + str(i) + " " +  " [ ] " + weight[y-1] + " ) "
+        else:
+            for y in layer[x]: #for each node
+                graphString+= "( m function " + str(i) + " " + bias[x][y-max(layer[x-1]) - 1] + " " + str(layer[y-1]).replace(",", " ").replace("[", "[ ").replace("]", " ] ")
+                for i in layer[x-1]: #for each input of the node
+                    if x==1:
+                        graphString += str(weight[x-1][i][y]) + " "
+                    else:
+                        graphString += str(weight[x-1][i-max(layer[x-2])-1][y]) + " "
+                graphString+=" ) "
+        x+=1
     print(graphString)
